0065239f1a2035dddeab4e85824cd2b9d1c7d8f1
Eldar Akavov <akavoveldar3005@gmail.com>
2020-09-14 02:49:48 +0300

    feat(script.js):new feat

--- script/script.js
+++ script/script.js
@@ -1,7 +1,49 @@
-'use strict';
+"use strict";
+// ДЗ 9
+const timeOne = document.querySelector('.time-one'),
+    timeTwo = document.querySelector('.time-two');
 
-let namePerson = prompt('Введите имя');
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
-let result = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
+const hourTransformation = item => {
+        if (item === 1 || item === 21) {
+            return 'час';
+        } else if (item === 0 || (item > 4 && item < 21)) {
+            return 'часов';
+        } else {
+            return 'часа';
+        }
+    },
+    minuteTransformation = item => {
+        if (item % 10 === 1 && item !== 11) {
+            return 'минута';
+        } else if ((item % 10 > 1 && item % 10 < 5) && (item < 10 || item > 20)) {
+            return 'минуты';
+        } else {
+            return 'минут';
+        }
+    },
+    secondTransformation = item => {
+        if (item % 10 === 1 && item !== 11) {
+            return 'секунда';
+        } else if ((item % 10 > 1 && item % 10 < 5) && (item < 10 || item > 20)) {
+            return 'секунды';
+        } else {
+            return 'секунд';
+        }
+    },
+    addZero = item => item < 10 ? '0' + item : item,
+    renderTime = () => {
+        let date = new Date();
 
-console.log(result);
+        let today = (date.getDay() !== 0) ? (date.getDay() - 1) : 6;
+
+
+        timeOne.textContent = `Сегодня ${week[today]}, ${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()} года, ${date.getHours()} ${hourTransformation(date.getHours())}  ${date.getMinutes()} ${minuteTransformation(date.getMinutes())} ${date.getSeconds()} ${secondTransformation(date.getSeconds())} `;
+
+        timeTwo.textContent = `${addZero(date.getDate())}.${addZero(date.getMonth())}.${date.getFullYear()}  - ${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(date.getSeconds())}`;
+
+    };
+
+setInterval(renderTime, 1000);
