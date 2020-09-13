f51e6c7187947f4194345f910218b3a7eb43bf6c
Eldar Akavov <akavoveldar3005@gmail.com>
2020-09-14 02:45:32 +0300

    feat(script.js):new feat

--- script/script.js
+++ script/script.js
@@ -1,35 +1,7 @@
-//Задача №1
-let lang = prompt('Введите название языка (ru/eng)');
+'use strict';
 
-const arrLang = [];
-arrLang['ru'] = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-arrLang['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let namePerson = prompt('Введите имя');
 
-if(lang === 'ru') {
-  alert('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-}else if (lang === 'en') {
-  alert('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-}else {
-  alert('что-то ввел не то');
-}
+let result = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
 
-switch(lang) {
-  case 'ru':
-    alert('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-    break;
-  case 'en':
-    alert('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-    break;
-  default:
-    alert('Что-то не то ввел');
-}
-
-console.log(arrLang[lang]);
-
-Заданик №2
-
-let namePerson = prompt('Введите имя')
-
-let result = namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'student';
-
-alert(result);
+console.log(result);
